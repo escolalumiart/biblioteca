4cf43317aa3f8074c8f71d926079276d10cf5cc2
edsonoliver <edsonoliver@brasnetec.com.br>
2019-04-30 07:34:02 -0300

    Publicaçao Inicial

--- ApaspBeneficios/Controllers/HomeController.cs
+++ ApaspBeneficios/Controllers/HomeController.cs
@@ -15,6 +15,11 @@ namespace ApaspBeneficios.Controllers
             return View();
         }
 
+        public IActionResult Beneficios()
+        {
+            return View();
+        }
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
